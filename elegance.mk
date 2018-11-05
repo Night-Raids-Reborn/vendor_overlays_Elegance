8a3ca93f731476c918a396fce20d08d38280096a
ElDainosor <eldainosor@gmail.com>
2018-11-05 14:22:04 +0530

    Elegance: Add more qs tiles style [2/3]
    Change-Id: I3ed890ff214cc70b7287517dc630ad6d3f6dcddb
    Signed-off-by: ROXAANE <bajithottyadvaith99@gmail.com>

--- elegance.mk
+++ elegance.mk
@@ -70,4 +70,9 @@ PRODUCT_PACKAGES += \
     QStileCircleTrim \
     QStileDefault \
     QStileDualToneCircle \
-    QStileSquircleTrim
+    QStileSquircleTrim \
+    QStileAttemptMountain \
+    QStileDottedCircle \
+    QStileNinja \
+    QStilePokesign \
+    QStileWavey
