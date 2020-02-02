3c5373bd4e3a3e100969cabc59903dbe30ef380c
NurKeinNeid <mralexman3000@gmail.com>
2020-02-02 15:20:20 +0100

    Elegance: Add Tinkerbell Font overlay
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>
    Signed-off-by: Aston-Martinn <advaithbhat9@gmail.com>

--- Fonts/fonts.mk
+++ Fonts/fonts.mk
@@ -9,6 +9,7 @@ PRODUCT_PACKAGES += \
     FontComicSansSourceOverlay \
     FontCoolstorySourceOverlay \
     FontFiraSansSourceOverlay \
+    FontTinkerbellSourceOverlay \
     FontGoogleSansSourceOverlay \
     FontGoogleSansMediumSourceOverlay \
     FontLGSmartGothicSourceOverlay \
@@ -37,6 +38,7 @@ PRODUCT_COPY_FILES += \
     vendor/overlays/Elegance/Fonts/prebuilt/FiraSans-Medium.ttf:$(TARGET_COPY_OUT_SYSTEM)/fonts/FiraSans-Medium.ttf \
     vendor/overlays/Elegance/Fonts/prebuilt/FiraSans-MediumItalic.ttf:$(TARGET_COPY_OUT_SYSTEM)/fonts/FiraSans-MediumItalic.ttf \
     vendor/overlays/Elegance/Fonts/prebuilt/FiraSans-Regular.ttf:$(TARGET_COPY_OUT_SYSTEM)/fonts/FiraSans-Regular.ttf \
+    vendor/overlays/Elegance/Fonts/prebuilt/GearTinkerbell.ttf:$(TARGET_COPY_OUT_SYSTEM)/fonts/GearTinkerbell.ttf \
     vendor/overlays/Elegance/Fonts/prebuilt/Gobold.ttf:$(TARGET_COPY_OUT_SYSTEM)/fonts/Gobold.ttf \
     vendor/overlays/Elegance/Fonts/prebuilt/GoogleSans-Bold.ttf:$(TARGET_COPY_OUT_SYSTEM)/fonts/GoogleSans-Bold.ttf \
     vendor/overlays/Elegance/Fonts/prebuilt/GoogleSans-BoldItalic.ttf:$(TARGET_COPY_OUT_SYSTEM)/fonts/GoogleSans-BoldItalic.ttf \
