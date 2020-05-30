9871b2797440bcb3342b69457791e8b09ce9ac13
DeadmanxXD <deadmanxxd@gmail.com>
2020-05-30 20:58:29 +0530

    Elegance: Add Elegant Transparent Theme [3/3]
    Change-Id: Icf2b61d95dc7b755c107e30999ccbed1fb45d85c
    Signed-off-by: Aston-Martinn <advaithbhat9@gmail.com>

--- Themes/themes.mk
+++ Themes/themes.mk
@@ -1,5 +1,7 @@
 # Themes
 PRODUCT_PACKAGES += \
+    ElegantThemeSystemOverlay \
+    ElegantThemeSystemUIOverlay \
     PitchBlackSystemOverlay \
     PitchBlackSystemUIOverlay \
     SolarizedDarkSystemOverlay \
