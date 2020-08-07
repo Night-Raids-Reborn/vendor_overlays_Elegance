8cc49cda4b3971c952970abe4152819df1b96368
ZeNiXxX <zenixxx.havoc@gmail.com>
2020-08-07 18:32:48 +0200

    Elegance: Introduce UI Style Picker [2/3]
    Signed-off-by: Aston-Martinn <advaithbhat9@gmail.com>

--- elegance.mk
+++ elegance.mk
@@ -119,3 +119,11 @@ PRODUCT_PACKAGES += \
     BrightnessSliderHalfGradient \
     BrightnessSliderMiniHalf \
     BrightnessSliderFlat
+
+# UI Styles
+PRODUCT_PACKAGES += \
+    UiStyleDefault \
+    UiStyleRoundLarge \
+    UiStyleRoundMedium \
+    UiStyleNoCornerRadius \
+    UiStyleRectangle
